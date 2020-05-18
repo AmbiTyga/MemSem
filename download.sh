1d7ca868490d6a8e327336d41e071e8edec0c91f
AmbiTyga <ambesh.sinha@gmail.com>
2020-05-18 23:40:54 +0530

    Added shell and requirements file

---
+++ download.sh
@@ -0,0 +1,3 @@
+googleimagesdownload --keywords "dark negative memes" --limit 2000
+googleimagesdownload --keywords "positive memes" --limit 2000
+googleimagesdownload --keywords "nonsense memes" --limit 2000
---
+++ install.sh
@@ -0,0 +1,8 @@
+pip install praw
+sudo apt install tesseract-ocr
+pip install pytesseract
+pip install transformers
+pip install tensorflow==2.1.0-rc0
+pip install pandas 
+pip install numpy
+pip install nltk
